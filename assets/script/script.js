03c6e66afc331597aac039661d6a22ad893a2aae
Nick Prater <NikPratr@gmail.com>
2022-09-26 18:17:55 -0400

    Work in progress. Currently working on adding all of the relevent data to the page upon searching a city

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,47 @@
-var oneCallAPI = "https://api.openweathermap.org/data/2.5/onecall/appid=5333f582a3686deab8dfbad18c0282ac";
-var APIKey = "cdce9d5ab609a9a2be9e26b244ddfc46"
+var APIKey = "cdce9d5ab609a9a2be9e26b244ddfc46";
+var cityName = document.querySelector("#city-name");
+var cityTemp = document.querySelector("#temp");
+var cityWind = document.querySelector("#wind");
+var cityHumidity = document.querySelector("#humidity");
+var cityUV = document.querySelector("#uv");
+var userInput = document.querySelector("#searchbar");
+var submitButton = document.querySelector("#submit-button");
+
+// submitButton.addEventListener("click", defineCity);
+
+// function defineCity() {
+//     var city = userInput.value;
+//     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+//     fetch(queryURL)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//         cityName.textContent = data.name;
+//         console.log(data.main.temp);
+//         cityTemp.textContent = "Temperature: " + data.main.temp + " K"
+//         // cityTemp.textContent = data.main[0];
+//     });
+// }
+
+var city = "Dayton";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    cityName.textContent = data.name;
+    console.log(data.main.temp);
+    var convertedTemp = ((data.main.temp-273.15)*1.8)+32;
+    cityTemp.textContent = "Temperature: " + convertedTemp + "°F";
+    // cityTemp.textContent = data.main[0];
+});
+
+// fetch(queryURL)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
